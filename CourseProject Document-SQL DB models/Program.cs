7965ebeb1858f8aff4a63517ddccd534cfc13032
FURRKA <alexsey.furrka@gmail.com>
2024-12-04 19:05:53 +0300

    Добавьте файлы проекта.

---
+++ CourseProject Document-SQL DB models/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CourseProject_Document_SQL_DB_models
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
